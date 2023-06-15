1b7222b3e9e69450474361bf8c77f66be747d89b
BrunoDark777 <106707123+BrunoDark777@users.noreply.github.com>
2023-06-15 19:02:53 -0300

    commit 2

--- src/pages/Video.js
+++ src/pages/Video.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import "./video.css"
 
 function Video() {
+  
+  const videoRef = useRef(null)
+  const [play, setplay] = useState(false)
+
+  function handdleStart(){
+
+    if(play){
+      videoRef.current.pause()
+      setplay(false)
+
+    } else {
+      videoRef.current.play()
+      setplay(true)
+
+    }
+
+
+  }
+
   return (
     <div className='video'>
-        Eu sou um componente Video
-        <video className='video__player'>
+
+        <video className='video__player' ref={videoRef} onClick={handdleStart} loop
+         src="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/brecker2.mp4?t=2023-05-22T19%3A37%3A45.885Z">
 
         </video>
     </div>
