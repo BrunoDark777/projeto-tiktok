7c25601dc554353040b2c630f5544b47b7554f75
BrunoDark777 <106707123+BrunoDark777@users.noreply.github.com>
2023-06-15 17:28:05 -0300

    primeiro commit conteudo

---
+++ src/pages/Video.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./video.css"
+
+function Video() {
+  return (
+    <div className='video'>
+        Eu sou um componente Video
+        <video className='video__player'>
+
+        </video>
+    </div>
+  )
+}
+
+export default Video
