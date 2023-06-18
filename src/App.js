03daf2409289703bac0979d1c4f4541d2d446e44
BrunoDark777 <106707123+BrunoDark777@users.noreply.github.com>
2023-06-18 18:10:02 -0300

    finalizando banco de dados

--- src/App.js
+++ src/App.js
@@ -1,48 +1,39 @@
+import React, { useEffect, useState } from "react";
 import "./App.css";
-import Video from "./pages/Video"
+import Video from "./pages/Video";
+import db from "./config/firebase";
+import { collection, getDocs } from "firebase/firestore/lite";
 
 function App() {
+  const [videos, setVideos] = useState([]);
+
+  async function getVideos() {
+    const videosCollection = collection(db, "videos");
+    const videosSnapshot = await getDocs(videosCollection);
+    const videosList = videosSnapshot.docs.map((doc) => doc.data());
+    setVideos(videosList);
+  }
+
+  useEffect(() => {
+    getVideos();
+  }, []);
+
   return (
     <div className="App">
       <div className="app__videos">
-        <Video
-        likes={100} 
-        messages={200}
-        shares={300}
-        name="@BrunoCésar"
-        description="Brecker o goleiro"
-        music="Musica epica"
-        url="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/brecker2.mp4?t=2023-05-22T19%3A37%3A45.885Z"
-
-        />
-        <Video 
-        likes={1500} 
-        messages={2346}
-        shares={370}
-        name="@paulosantos2058"
-        description="Bird olhando para a camera"
-        music="Clap your hands"
-        url="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/bird.mp4?t=2023-05-22T19%3A40%3A47.052Z"
-
-        />
-        <Video
-        likes={10} 
-        messages={56}
-        shares={25}
-        name="@BrunoCésar"
-        description="Brecker o goleiro"
-        music="musica epica"
-
-         />
-        <Video
-        likes={999} 
-        messages={150}
-        shares={555}
-        name="@BrunoCésar"
-        description="Brecker o goleiro"
-        music="musica epica"
-
-         />
+        {videos.map((itens) => {
+          return (
+            <Video
+              likes={itens.likes}
+              messages={itens.messages}
+              shares={itens.shares}
+              name={itens.name}
+              description={itens.description}
+              music={itens.music}
+              url={itens.url}
+            />
+          );
+        })}
       </div>
     </div>
   );
--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,11 +1,8 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from 'firebase/firestore/lite';
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
   apiKey: "AIzaSyBBtr12ErtKnPGsffRkNrne_djY4uK7Z14",
   authDomain: "tiktok-jornada-40e59.firebaseapp.com",
@@ -16,6 +13,8 @@ const firebaseConfig = {
   measurementId: "G-SE6ZBM3HM3"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const db = getFirestore(app)
+
+export default db
